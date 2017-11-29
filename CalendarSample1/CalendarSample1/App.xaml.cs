aa875e325af34ffd1918a7edc55fe9b3fc6cdd5b
柊 佐藤 <150036@jc-21.jp>
2017-11-29 14:48:40 +0900

    修正６

--- CalendarSample1/CalendarSample1/App.xaml.cs
+++ CalendarSample1/CalendarSample1/App.xaml.cs
@@ -15,11 +15,20 @@ namespace CalendarSample1
 
             MainPage = new CalendarSample1.MainPage();
         }
-        protected override void ();
+
+        protected override void OnStart()
         {
-           
+            // Handle when your app starts
         }
 
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
     }
 }
